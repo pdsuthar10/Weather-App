6d99ba5cd7e9d267a0cea6c1edaac195ba28549b
Priyam Suthar <suthar.p@husky.neu.edu>
2020-08-26 04:59:32 -0400

    Updates

---
+++ precache-manifest.5fc1122ec895a4bd99c4136a4c44cc59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f895b4f8c62e24c62238db87bdc0f66",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "f692990fd489f90c7337",
+    "url": "/Weather-App/static/css/main.413649a8.chunk.css"
+  },
+  {
+    "revision": "be6338d2ffce2758475a",
+    "url": "/Weather-App/static/js/2.d8a08eb1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Weather-App/static/js/2.d8a08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f692990fd489f90c7337",
+    "url": "/Weather-App/static/js/main.794453b9.chunk.js"
+  },
+  {
+    "revision": "cd28dbe1978c23885ff4",
+    "url": "/Weather-App/static/js/runtime-main.af0d9584.js"
+  },
+  {
+    "revision": "bf33a3798c6289a4410490bd00ea1221",
+    "url": "/Weather-App/static/media/back2.bf33a379.jpg"
+  },
+  {
+    "revision": "3c74c4aaa780f86af49334822b2d3396",
+    "url": "/Weather-App/static/media/clouds.3c74c4aa.jpg"
+  },
+  {
+    "revision": "7b80ac90146da3be27dee776459eff36",
+    "url": "/Weather-App/static/media/currentBackground6.7b80ac90.jpg"
+  },
+  {
+    "revision": "6741d893c9977207456f8c3d93cb619d",
+    "url": "/Weather-App/static/media/drizzle.6741d893.jpg"
+  },
+  {
+    "revision": "2888699c58f527f17347749ff00abab2",
+    "url": "/Weather-App/static/media/snow.2888699c.png"
+  },
+  {
+    "revision": "7bb0848b247bb785f352a25ecf186d24",
+    "url": "/Weather-App/static/media/thunderstorm.7bb0848b.png"
+  }
+]);
